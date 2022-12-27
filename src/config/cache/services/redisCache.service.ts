8d546af522a66cebf5b0aed8bbfb413fdaf9622a
Kronnos <ing.korozco@gmail.com>
2022-12-27 09:01:55 -0500

    First Commit

---
+++ src/config/cache/services/redisCache.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, Inject, CACHE_MANAGER } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+
+@Injectable()
+export class RedisCacheService {
+  constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
+
+  async get(key: string): Promise<string> {
+    return await this.cache.get(key);
+  }
+
+  async set(key: string, value: any, expired?: number) {
+    await this.cache.set(key, value, { ttl: expired });
+  }
+
+  async del(key: string) {
+    await this.cache.del(key);
+  }
+
+  async reset() {
+    await this.cache.reset();
+  }
+}
