33df0090a77492faa07d1c4d3c2a987d089db331
KristianOrozco <kristian.orozco@infracommerce.lat>
2023-03-03 19:11:37 -0500

    add controllers, service, dto, entity and module blank

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 import { RedisCacheModule } from './config/cache/redisCache.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AppConfigModule, RedisCacheModule],
+  imports: [AppConfigModule, RedisCacheModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
